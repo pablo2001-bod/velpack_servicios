75eae11958ac253a426d1f7d03b28e55e935bac7
Pablo Tisalema <pablo.tisalema9488@utc.edu.ec>
2026-08-06 20:29:35 -0500

    Actualizando ALLOWED_HOSTS para Render

--- config/settings.py
+++ config/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-&dqh^6imt3^zsixtxr$%u)gxpr$nre$ya%%1_l4bbgzi8++u09
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
